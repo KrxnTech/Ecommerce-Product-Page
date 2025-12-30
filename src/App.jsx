811000aba14ba4958bf085a4b7e681e6439aafdf
KRISHNA-ai-stack <sharmakrishna1142@gmail.com>
2025-12-30 17:03:33 +0530

    Added Diff image's of L

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,10 @@ function App() {
       <Navbar />
       <Heading />
       <div className="PARENTOP">
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+        <ProductCard img="https://i.pinimg.com/originals/44/fa/a6/44faa65bc57e20d1c8e4abb3e72c0429.jpg"/>
+        <ProductCard img="https://64.media.tumblr.com/fd3e1d35b81213f038c653d35d504689/6350ec158e715afb-6c/s640x960/f993888b1aa6955196993302482478a77a99af40.pnj"/>
+        <ProductCard img="https://froheyo.com/cdn/shop/files/ANI05_L-DeathNote.jpg?v=1732360353"/>
+        <ProductCard img="https://i.pinimg.com/originals/e5/b1/c7/e5b1c762f752a92c003ecd84b47628e4.jpg"/>
       </div>
     </>
   )
--- src/Component/Products-Box/Product-Card.jsx
+++ src/Component/Products-Box/Product-Card.jsx
@@ -1,10 +1,10 @@
 import "./Product-Card.css"
 
-function ProductCard() {
+function ProductCard({img}) {
     return (
         <div className="PARENTOP">
             <div className="PRODUCTCARD">
-                <img src="https://i.pinimg.com/originals/44/fa/a6/44faa65bc57e20d1c8e4abb3e72c0429.jpg" alt="" />
+                <img src= {img} alt="" />
             </div>
         </div>
     )
